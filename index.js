d34342145e8373f1e044b1b6c497b69a3cc0d63c
Mike Loseto <losetom1998@gmail.com>
2022-07-27 16:06:21 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    console.log(document.querySelector('p'))
+    document.getElementById('text').innerHTML = "This is really cool!"
+  });
+    console.log("Before DOM has loaded")
+
